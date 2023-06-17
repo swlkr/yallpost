29c9cdb4a8cf2135f25232b0ffe5c077a2042637
Sean Walker <swlkr.rbl@gmail.com>
2023-06-17 15:35:54 -0600

    Move backend under backend feature

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
+/*
+    TODO: move backend to backend
+    TODO: add dioxus
+    TODO: server fns
+    TODO: env vars
+    TODO: sqlx
+    TODO: favicons
+    TODO: meta tags
+    TODO: assets
+    TODO: cache busting assets
+*/
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(render));
-    let addr: SocketAddr = "127.0.0.1:9004".parse().expect("Problem parsing address");
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .expect("Problem starting axum");
+fn main() {
+    #[cfg(feature = "backend")]
+    backend::main();
 }
 
-async fn render() -> Html<&'static str> {
-    Html("<h1>Hello, world!</h1>")
+#[cfg(feature = "backend")]
+mod backend {
+    use axum::{response::Html, routing::get, Router, Server};
+    use std::net::SocketAddr;
+
+    #[tokio::main]
+    pub async fn main() {
+        let app = Router::new().route("/", get(render));
+        let addr: SocketAddr = "127.0.0.1:9004".parse().expect("Problem parsing address");
+        println!("listening on {}", addr);
+        Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await
+            .expect("Problem starting axum");
+    }
+
+    async fn render() -> Html<&'static str> {
+        Html("<h1>Yo from the backend</h1>")
+    }
 }
