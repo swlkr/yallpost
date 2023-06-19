4190777ca0c728ab343ab5b4d44a272da3792c6a
Sean Walker <swlkr.rbl@gmail.com>
2023-06-19 15:08:48 -0600

    Add dioxus web + hydration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 #![allow(non_snake_case)]
 /*
-    TODO: add dioxus ssr
-    TODO: static assets
-    TODO: cache busting assets
+    TODO: add dioxus web
     TODO: favicons
     TODO: meta tags
-    TODO: add dioxus
     TODO: server fns
     TODO: env vars
     TODO: sqlx
@@ -13,8 +10,17 @@
 use dioxus::prelude::*;
 
 fn main() {
+    #[cfg(feature = "frontend")]
+    frontend::main();
     #[cfg(feature = "backend")]
     backend::main();
+}
+
+#[cfg(feature = "frontend")]
+mod frontend {
+    pub fn main() {
+        dioxus_web::launch_with_props(super::App, (), dioxus_web::Config::default().hydrate(true));
+    }
 }
 
 #[cfg(feature = "backend")]
@@ -55,9 +61,18 @@ mod backend {
 
     async fn index(State(state): State<AppState>) -> Html<String> {
         let AppState { assets } = state;
-        let mut vdom = VirtualDom::new_with_props(Layout, LayoutProps { assets });
+        let mut vdom = VirtualDom::new_with_props(App, ());
         let _ = vdom.rebuild();
-        Html(format!("<!DOCTYPE html>{}", dioxus_ssr::render(&vdom)))
+        let app = dioxus_ssr::pre_render(&vdom);
+        Html(format!(
+            "<!DOCTYPE html>{}",
+            dioxus_ssr::render_lazy(rsx! {
+                Layout {
+                    assets: assets
+                    app: app,
+                }
+            })
+        ))
     }
 
     async fn serve_assets(uri: Uri) -> impl IntoResponse {
@@ -74,15 +89,19 @@ mod backend {
 
     #[inline_props]
     fn Head<'a>(cx: Scope, assets: &'a Vec<Asset>) -> Element {
-        let assets = assets.iter().map(|a| match a.ext {
-            Ext::Css => rsx! { link { rel: "stylesheet", href: "{a}" } },
-            Ext::Js => rsx! { script { src: "{a}", defer: true } },
-            Ext::Unknown => rsx! { () },
-        });
+        let assets = assets
+            .iter()
+            .filter(|a| !a.path.contains("dioxus"))
+            .map(|a| match a.ext {
+                Ext::Css => rsx! { link { rel: "stylesheet", href: "{a}" } },
+                Ext::Js => rsx! { script { src: "{a}", defer: true } },
+                Ext::Unknown => rsx! {()},
+            });
         cx.render(rsx! {
             head {
-                meta { charset: "utf-8" }
-                meta { name: "viewport", content: "width=device-width" }
+                meta { charset: "UTF-8" }
+                meta { name: "viewport", content: "width=device-width, initial-scale=1" }
+                meta { content: "text/html;charset=utf-8", http_equiv: "Content-Type" }
                 title { "mozzzz" }
                 assets
             }
@@ -92,14 +111,34 @@ mod backend {
     #[derive(Props, PartialEq)]
     struct LayoutProps {
         assets: Vec<Asset>,
+        app: String,
     }
 
     fn Layout(cx: Scope<LayoutProps>) -> Element {
-        let LayoutProps { assets } = cx.props;
+        let LayoutProps { assets, app } = cx.props;
+        let x: Vec<u64> = assets
+            .iter()
+            .filter(|a| a.path.contains("dioxus.js") || a.path.contains("dioxus_bg.wasm"))
+            .map(|a| a.last_modified)
+            .collect();
+        let js = if let (Some(a), Some(b)) = (x.get(0), x.get(1)) {
+            format!(
+                r#"import init from "/./assets/dioxus/dioxus.js?v={}";
+                   init("/./assets/dioxus/dioxus_bg.wasm?v={}").then(wasm => {{
+                     if (wasm.__wbindgen_start == undefined) {{
+                       wasm.main();
+                     }}
+                   }});"#,
+                a, b
+            )
+        } else {
+            Default::default()
+        };
         cx.render(rsx! {
             Head { assets: assets }
             body {
-                main { id: "main", App {} }
+                div { id: "main", "{app}" }
+                script { r#type: "module", "{js}" }
             }
         })
     }
@@ -107,6 +146,6 @@ mod backend {
 
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div { class: "h-screen dark:bg-gray-950 dark:text-white text-gray-950", "Yo from dioxus ssr" }
+        div { class: "h-screen dark:bg-gray-950 dark:text-white text-gray-950", "yo from dioxus hydrate"}
     })
 }
